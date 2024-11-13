ded0c016c6e3e10f0d34baaebe52f79e857bd0f8
sophia-1234567 <sophiagogliashvili92@gmail.com>
2024-11-13 22:57:15 +0400

    update commit

---
+++ classworks/classwork .py
@@ -0,0 +1,16 @@
+barbare = 25          
+giorgi = 12.5        
+levani = "Hello"     
+
+# Changing types
+barbare = str(barbare)    
+giorgi = int(giorgi)       
+levani = float(len(levani)) 
+
+print(barbare)
+print(giorgi)
+print(levani)
+
+barbare = 10
+giorgi = 34 
+leavni = 35
---
+++ classworks/classwork.py
@@ -0,0 +1,11 @@
+print ("sophia")
+print (10)
+print (40.07)
+
+print ("jarji")
+print (6)
+print (20.07)
+
+print ("levani")
+print (35)
+print (67.05)  
