37dfba1b259bb201ff3ed763ef110f1ee2962233
sophia-1234567 <sophiagogliashvili92@gmail.com>
2024-12-05 21:07:43 +0400

    update commit

---
+++ level 13/homework/homework.py
@@ -0,0 +1,10 @@
+("GOA BEST") # true or false
+("YOU WILL BECOME CHAD") # true or false
+("YOU ARE THE BEST") # true or false
+("BATONI GURAM BESR") # true or false
+("FOREVER GOA STUDENT") #true or false
+("YOU WILL NEVER GIVE UP") #true or false
+("NIKA KESHELAVA BEST") #true or false
+("CHAD") #true or false
+("you are pretty") #true or false
+("GOA BEST💯🔥😎")
